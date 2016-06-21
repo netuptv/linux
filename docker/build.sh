9eb2969719bfa1927a902d51c8c464ee77c5a0af
Vladimir Kaunkin <kvi@netup.ru>
2016-06-21 12:24:34 +0300

    build script fixes for jenkins

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,12 @@ set -ex
 
 SCRIPT_DIR=$(cd $(dirname ${0}); pwd)
 SRC_DIR=${SCRIPT_DIR}/..
+if [ "${JENKINS_HOME}" ]; then
+    TTY_ARGS="-t"
+    cd "${JENKINS_HOME}"/builds
+else
+    TTY_ARGS="-it"
+fi
 BUILD_DIR=$(pwd)/build/
 OUT_DIR=$(pwd)/out/
 
@@ -13,8 +19,9 @@ IMAGE_NAME=build.netup:5000/iptv_2.0_build:${IMAGE_TAG}
 mkdir -p ${BUILD_DIR} ${OUT_DIR}
 
 #    --volume ${BUILD_DIR}:/mnt/build \
-docker run --rm -it \
+docker run --rm \
     --volume ${SRC_DIR}:/mnt/src \
     --volume ${OUT_DIR}:/mnt/out \
+    ${TTY_ARGS} \
     ${IMAGE_NAME} \
     /mnt/src/docker/scripts/make.sh
