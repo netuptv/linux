f699eabe6945a1bf14a6679b99d66f3d51d1b3dd
Vladimir Kaunkin <kvi@netup.ru>
2016-07-11 10:46:32 +0300

    reverted build scripts and configs, see T19

--- docker/build.sh
+++ docker/build.sh
@@ -5,23 +5,22 @@ set -ex
 SCRIPT_DIR=$(cd $(dirname ${0}); pwd)
 SRC_DIR=${SCRIPT_DIR}/..
 if [ "${JENKINS_HOME}" ]; then
-    TTY_ARGS="-t"
-    cd "${JENKINS_HOME}"/builds
-else
-    TTY_ARGS="-it"
+    mkdir -p jenkins
+    cd jenkins
 fi
-BUILD_DIR=$(pwd)/build/
-OUT_DIR=$(pwd)/out/
-
+OUT_DIR=$(pwd)/out/linux-4.4
+REVISION=$(cd ${SRC_DIR}; git rev-parse HEAD)
 IMAGE_TAG=$(head -n 1 ${SCRIPT_DIR}/build.tag)
 IMAGE_NAME=build.netup:5000/iptv_2.0_build:${IMAGE_TAG}
 
-mkdir -p ${BUILD_DIR} ${OUT_DIR}
+mkdir -p ${OUT_DIR}
 
-#    --volume ${BUILD_DIR}:/mnt/build \
 docker run --rm \
     --volume ${SRC_DIR}:/mnt/src \
     --volume ${OUT_DIR}:/mnt/out \
-    ${TTY_ARGS} \
+    --user ${UID} \
+    -t \
     ${IMAGE_NAME} \
     /mnt/src/docker/scripts/make.sh
+
+printf "kernel_44_revision=%s\n" "${REVISION}" > ${OUT_DIR}/build.info
