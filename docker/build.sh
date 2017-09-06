a8494fb2b182dfff8881c430bbe114a811baed36
kvi <kvi@netup.ru>
2017-09-06 14:30:23 +0300

    update multibranch

--- docker/build.sh
+++ docker/build.sh
@@ -4,16 +4,12 @@ set -e
 
 SCRIPT_DIR=$(cd $(dirname ${0}); pwd)
 SRC_DIR=${SCRIPT_DIR}/..
-if [ "${JENKINS_HOME}" ]; then
-    mkdir -p jenkins
-    cd jenkins
-fi
-BUILD_DIR=$(pwd)/build/linux-4.4
-CCACHE_DIR=$(pwd)/ccache/linux-4.4
-OUT_DIR=$(pwd)/out/linux-4.4
+BUILD_DIR=$(pwd)/build/kernel
+CCACHE_DIR=$(pwd)/ccache/kernel
+OUT_DIR=$(pwd)/out/kernel
 REVISION=$(cd ${SRC_DIR}; git rev-parse HEAD)
 [ -z "${BRANCH_NAME}" ] && \
-    BRANCH_NAME=$(cd ${SRC_DIR}; git symbolic-ref --short HEAD)
+    BRANCH_NAME=$(cd ${SRC_DIR}; git symbolic-ref -q --short HEAD || echo unknown)
 IMAGE_TAG=$(head -n 1 ${SCRIPT_DIR}/build.tag)
 IMAGE_NAME=build.netup:5000/iptv_2.0_build:${IMAGE_TAG}
 
@@ -28,4 +24,4 @@ docker run --rm -t \
     ${IMAGE_NAME} \
     /mnt/src/docker/scripts/make.sh
 
-printf 'kernel_44_revision="%s %s %s"\n' "${REVISION}" "${BRANCH_NAME}" "${BUILD_URL}" > ${OUT_DIR}/build.info
+printf 'kernel_revision="%s %s %s"\n' "${REVISION}" "${BRANCH_NAME}" "${BUILD_URL}" > ${OUT_DIR}/build.info
