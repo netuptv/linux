79ca8befb54da3bff9ffbe1a37dff31ada78e43e
kvi <kvi@netup.ru>
2018-04-20 16:11:37 +0300

    ref T2916, build perf statically

--- docker/scripts/make.sh
+++ docker/scripts/make.sh
@@ -6,6 +6,7 @@ SRC_DIR=$(cd $(dirname ${0})/../..; pwd)
 BUILD_DIR=/mnt/build
 CCACHE_DIR=/mnt/ccache
 OUT_DIR=/mnt/out/
+PERF_BUILD_DIR=${BUILD_DIR}/tools/perf
 
 export PATH=/usr/lib/ccache/:${PATH}
 export CCACHE_DIR
@@ -20,5 +21,9 @@ TAR_FILE=${BUILD_DIR}/linux-${RELEASE}-x86.tar
 
 mv ${TAR_FILE} ${OUT_DIR}/linux-4.4.tar
 
-make tools/perf O=${BUILD_DIR}
-cp ${BUILD_DIR}/tools/perf/perf ${OUT_DIR}/
+mkdir -p ${PERF_BUILD_DIR}
+(
+    cd tools/perf
+    make LDFLAGS=-static O=${PERF_BUILD_DIR}
+)
+cp ${PERF_BUILD_DIR}/perf ${OUT_DIR}/
