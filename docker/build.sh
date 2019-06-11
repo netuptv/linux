5a6b9cf785f415bae0f6321113e01a5526fae61c
kvi <kvi@netup.ru>
2019-06-11 13:21:20 +0300

    :pencil: (build) create image at build, ref T4640
    squashed: :pencil: (build): fix package name, ref T4640
    
    auditors: alex3d

--- docker/build.sh
+++ docker/build.sh
@@ -2,26 +2,39 @@
 
 set -e
 
-SCRIPT_DIR=$(cd $(dirname ${0}); pwd)
-SRC_DIR=${SCRIPT_DIR}/..
-BUILD_DIR=$(pwd)/build/kernel
-CCACHE_DIR=$(pwd)/ccache/kernel
-OUT_DIR=$(pwd)/out/kernel
-REVISION=$(cd ${SRC_DIR}; git rev-parse HEAD)
+SCRIPT_DIR="$(cd "$(dirname "${0}")"; pwd)"
+SRC_DIR="${SCRIPT_DIR}/.."
+BUILD_DIR="$(pwd)/build/kernel"
+CCACHE_DIR="$(pwd)/ccache/kernel"
+OUT_DIR="$(pwd)/out/kernel"
+REVISION="$(cd "${SRC_DIR}"; git rev-parse HEAD)"
 [ -z "${BRANCH_NAME}" ] && \
-    BRANCH_NAME=$(cd ${SRC_DIR}; git symbolic-ref -q --short HEAD || echo unknown)
-IMAGE_TAG=$(head -n 1 ${SCRIPT_DIR}/build.tag)
-IMAGE_NAME=build.netup:5000/iptv_2.0_build:${IMAGE_TAG}
+    BRANCH_NAME="$(cd "${SRC_DIR}"; git symbolic-ref -q --short HEAD || echo unknown)"
 
-mkdir -p ${OUT_DIR} ${BUILD_DIR} ${CCACHE_DIR}
+mkdir -p "${OUT_DIR}" "${BUILD_DIR}" "${CCACHE_DIR}"
+
+. "${SCRIPT_DIR}/image/image.env"
+SNAPSHOT_URL="http://snapshot.debian.org/archive/debian/${SNAPSHOT}"
+
+docker build --force-rm --iidfile "${BUILD_DIR}/image.id" - <<EOF
+FROM debian:${IMAGE_TAG}
+RUN echo 'deb ${SNAPSHOT_URL} stretch main' > /etc/apt/sources.list && \
+    apt-get update && \
+    apt-get install --no-install-recommends --assume-yes \
+        gcc make flex bison ccache bc xz-utils \
+        libc6-dev libssl-dev libelf-dev binutils-dev liblzma-dev libnuma-dev \
+        zlib1g-dev && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+EOF
 
 docker run --rm -t \
-    --volume ${SRC_DIR}:/mnt/src:ro \
-    --volume ${BUILD_DIR}:/mnt/build \
-    --volume ${CCACHE_DIR}:/mnt/ccache \
-    --volume ${OUT_DIR}:/mnt/out \
-    --user ${UID} \
-    ${IMAGE_NAME} \
+    --volume "${SRC_DIR}:/mnt/src:ro" \
+    --volume "${BUILD_DIR}:/mnt/build" \
+    --volume "${CCACHE_DIR}:/mnt/ccache" \
+    --volume "${OUT_DIR}:/mnt/out" \
+    --user "${UID}" \
+    "$( head -n1 "${BUILD_DIR}/image.id" )" \
     /mnt/src/docker/scripts/make.sh
 
-printf 'kernel_revision="%s %s %s"\n' "${REVISION}" "${BRANCH_NAME}" "${BUILD_URL}" > ${OUT_DIR}/build.info
+printf 'kernel_revision="%s %s %s"\n' "${REVISION}" "${BRANCH_NAME}" "${BUILD_URL}" > "${OUT_DIR}/build.info"
