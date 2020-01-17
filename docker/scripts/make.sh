5a1ef9a859fd71552a349455cf5778af1075a4f0
Alexey V. Butkeev <abutkeev@netup.ru>
2020-01-17 22:05:18 +0300

    :sparkles: build deb packages; ref T4408
    squashed:
    - :pencil: add build user in container; ref T4408
    - :pencil: permissions fix
    - :pencil: revert create user and chown /mnt, move build dir one level deeper
    
    auditors: abutkeev

--- docker/scripts/make.sh
+++ docker/scripts/make.sh
@@ -6,20 +6,23 @@ SRC_DIR=$(cd $(dirname ${0})/../..; pwd)
 BUILD_DIR=/mnt/build
 CCACHE_DIR=/mnt/ccache
 OUT_DIR=/mnt/out/
-PERF_BUILD_DIR=${BUILD_DIR}/tools/perf
+PERF_BUILD_DIR="${BUILD_DIR}/make/tools/perf"
 
 export PATH=/usr/lib/ccache/:${PATH}
 export CCACHE_DIR
 
-cp ${SRC_DIR}/config.2.0 ${BUILD_DIR}/.config
+mkdir -p "${BUILD_DIR}/make"
+cp "${SRC_DIR}/config.2.0" "${BUILD_DIR}/make/.config"
 
 cd ${SRC_DIR}
-make -j $(nproc) O=${BUILD_DIR} tar-pkg
+make -j "$(nproc)" O="${BUILD_DIR}/make" tar-pkg
+make -j "$(nproc)" O="${BUILD_DIR}/make" DEBEMAIL='build <info@netup.ru>' bindeb-pkg
 
-RELEASE=$(cat ${BUILD_DIR}/include/config/kernel.release)
-TAR_FILE=${BUILD_DIR}/linux-${RELEASE}-x86.tar
+RELEASE="$(cat ${BUILD_DIR}/make/include/config/kernel.release)"
+TAR_FILE="${BUILD_DIR}/make/linux-${RELEASE}-x86.tar"
 
 mv ${TAR_FILE} ${OUT_DIR}/linux-4.19.tar
+mv ${BUILD_DIR}/*.deb ${OUT_DIR}/
 
 mkdir -p ${PERF_BUILD_DIR}
 (
