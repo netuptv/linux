16667439e70a6112d70d1795e9cf5ab42584091e
kvi <kvi@netup.ru>
2021-03-22 14:22:09 +0300

    :pencil: fix build scripts for 5.11; ref T6683
    auditors: alex3d

--- docker/scripts/make.sh
+++ docker/scripts/make.sh
@@ -5,7 +5,7 @@ set -e
 SRC_DIR=$(cd $(dirname ${0})/../..; pwd)
 BUILD_DIR=/mnt/build
 CCACHE_DIR=/mnt/ccache
-OUT_DIR=/mnt/out/
+OUT_DIR=/mnt/out
 PERF_BUILD_DIR="${BUILD_DIR}/make/tools/perf"
 
 export PATH=/usr/lib/ccache/:${PATH}
@@ -18,9 +18,11 @@ cd ${SRC_DIR}
 make -j "$(nproc)" O="${BUILD_DIR}/make" tar-pkg
 
 RELEASE="$(cat ${BUILD_DIR}/make/include/config/kernel.release)"
+KERNEL_VERSION="${RELEASE%%-*}"
+OUT_VERSION="${KERNEL_VERSION%.*}"
 TAR_FILE="${BUILD_DIR}/make/linux-${RELEASE}-x86.tar"
-
-mv ${TAR_FILE} ${OUT_DIR}/linux-5.4.tar
+OUT_FILE="${OUT_DIR}/linux-${OUT_VERSION}.tar"
+mv "${TAR_FILE}" "${OUT_FILE}"
 
 mkdir -p ${PERF_BUILD_DIR}
 (
